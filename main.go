d7b31bba7c36187bed27e0185329026ee97e4201
beekter <beekter@gmail.com>
2021-11-27 08:09:31 +0900

    * almost

--- main.go
+++ main.go
@@ -5,21 +5,19 @@ import (
 )
 
 func main() {
-	inputData := []int{0, 1, 3}
+	inputData := []int{0, 1}
 
 	inputJob := job(func(in, out chan interface{}) {
 		for _, fibNum := range inputData {
 			out <- fibNum
 		}
-
-		close(out)
 	})
 
 	outputJob := job(func(in, out chan interface{}) {
 		dataRaw := <-in
 		data, ok := dataRaw.(string)
 		if !ok {
-			fmt.Println("cant convert result data to string")
+			fmt.Println("cant convert Result Data to string")
 			return
 		}
 		fmt.Println(data)
@@ -32,6 +30,4 @@ func main() {
 		CombineResults,
 		outputJob,
 	)
-
-	fmt.Scanln()
 }
